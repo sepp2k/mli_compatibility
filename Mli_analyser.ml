dc2269eb1a2e2ab93af58976826a84b0553fbb0f
Sebastian Hungerecker <sepp2k@googlemail.com>
2018-12-01 15:33:06 +0100

    Read file contents into map from name to type

--- Mli_analyser.ml
+++ Mli_analyser.ml
@@ -1,13 +1,22 @@
 open Ocaml_common
+open Parsetree
+
+module StringMap = Map.Make(String)
 
 let parse_mli file =
-  let in_chan = open_in file in
-  let lexbuf = Lexing.from_channel in_chan in
-  let ast = Parser.interface Lexer.token lexbuf in
-  Pprintast.signature Format.std_formatter ast;
-  Format.print_newline ();
-  close_in in_chan;
-  ast
+    let in_chan = open_in file in
+    let lexbuf = Lexing.from_channel in_chan in
+    let ast = Parser.interface Lexer.token lexbuf in
+    close_in in_chan;
+    ast
+
+let create_map (f: 'a -> (string * 'b) option) (xs: 'a list): 'b StringMap.t =
+    let folder acc x =
+        match f x with
+        | Some (key, value) -> StringMap.add key value acc
+        | None -> acc
+    in
+    List.fold_left folder StringMap.empty xs
 
 let () =
     if Array.length Sys.argv <> 3
@@ -19,3 +28,10 @@ let () =
 let () = Lexer.init()
 let ast1 = parse_mli Sys.argv.(1)
 let ast2 = parse_mli Sys.argv.(2)
+let get_name_and_type_of_val (item: signature_item): (string * core_type) option =
+    match item.psig_desc with
+    | Psig_value value_desc -> Some (value_desc.pval_name.txt, value_desc.pval_type)
+    | _ -> None
+
+let file1_val_names_to_types = create_map get_name_and_type_of_val ast1
+let () = StringMap.iter (fun name typ -> Format.printf "%s: %a@." name Pprintast.core_type typ) file1_val_names_to_types
